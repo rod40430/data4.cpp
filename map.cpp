b0f9f5adca6bbe328ef578f0c726ad4c02980712
xogns <xogns40430@naver.com>
2024-02-02 23:48:52 +0900

    map의 선언법
    첫 번째 타입의 값은 Key라고 불린다
    두 번째 타입은 Value또는 맵핑된 타입의 값을 반환한다
    map의 첨자 연산은 본질적으로는 우리가 get_numver()라고 부르는
    검색이다 Key가 발견되지 않으면 Key는 자신의 value에 해당하는
    기본 값과 함깨 map에 입력된다 정수 타입의 기본 값은 0이다
    유효하지 않은 값이 입력되지 않도록 막고 싶다면 []대신에 find()와
    insert()를 사용할 수 있다.

---
+++ map.cpp
@@ -0,0 +1,9 @@
+#include <map>
+#include <string>
+using namespace std;
+map<string, int> phone_book
+{
+  {"David Hume", 123456},
+  {"Karl Popper", 234567},
+  {"Bertrand Arthur William Russell", 345678}
+};
