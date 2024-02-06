8a60d3765db8fbc776dd26a51ee87331e0fc3a2e
xogns <xogns40430@naver.com>
2024-02-06 22:14:35 +0900

    async는 thread보다 안정적이며 프로그래머가 사용하기 편리한 기능입니다.

--- async.cpp
+++ async.cpp
@@ -25,3 +25,12 @@ double comp4(vector<double>& v)
 
   return f0.get() + f1.get() + f2.get() + f3.get();
 }
+
+// 1. std::async에 대한 간단한 설명.
+// std::async는 std::task 클래스 기반으로 만들어진 클래스로 Thread를 만들 때 사용됩니다.
+
+// std::async는 std::thread와 달리 내부적으로 Thread Pool을 만들어 Thread를 관리하게 되며,
+
+// 예외처리, 값 return 등.... std::thread보다 안정적이며 프로그래머가 사용하기 편리한 기능입니다.
+
+// std::async는 반환 값을 std::future로 받습니다.
