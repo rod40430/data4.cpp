1686dd85307dc7e9178561c86bbbf407f18d4fa9
xogns <xogns40430@naver.com>
2024-02-07 20:46:54 +0900

    iterator_traits에 대한 설명

---
+++ 임의 접근_순방향 접근.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+using namespace std;
+
+template<typename C>
+using value_type = typename C::value_type;
+
+template<typename Ran>  // 임의 접근 반복자용 
+void sort_helper(Ran beg, Ran end, random_access_iterator_tag)  // [ beg:end]로 첨자 연산을
+// 할 수 있다.
+{
+  sort(beg,end);  // 단순 정렬한다
+}
+
+template<typename For>  // 순방향 연산자용
+void sort_helper(For beg, For end, forward_iterator_tag)  // [ beg:end]를 순회 탐색할 수 있다
+{
+  vector<value_type<For>> v {beg, end};  // [beg:end]의 벡터를 초기화한다
+  sort(v.begin, v.end());
+  copy(v.begin(), v.end(), beg);  // 원소를 되돌려서 복사한다
+}
