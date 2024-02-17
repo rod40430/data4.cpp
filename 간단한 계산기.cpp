27c616cb4a8f48ccb54fbf23f0eab2f36c4e9cf4
xogns <xogns40430@naver.com>
2024-02-17 22:31:37 +0900

    이 책에서 설명하는 계산기 만드는 코드이다
    이 코드를 해석하면서 좋은 방식은 훔친다

---
+++ 간단한 계산기.cpp
@@ -0,0 +1,97 @@
+enum class Kind : char
+{
+  name, number, end,
+  plus = '+', minus = '-', mul = '*', div = '/', print = ';', assign = '=',
+  lp = '(', rp =')'
+};
+
+struct Token
+{
+  Kind kind;
+  string string_value;
+  double number_value;
+};
+
+class ToKen_stream
+{
+public:
+  Token get();  // 다음 토큰을 읽고 반환한다
+  const Token& current();  // 가장 최근에 읽혀진 토큰
+  // ...
+};
+
+double expr(bool get)  // 덧셈과  뺄셈
+{
+  double left = term(get);
+
+  for (;;)  // 무한루프
+    {
+      switch (ts.current().kind))
+        {
+          case Kind::plus:
+            left += term(true);
+            break;
+          case Kind::minus:
+            left -= term(true);
+            break;
+          default:
+            return left;
+        }
+    }
+}
+
+double term(bool get)  // 곱셈과 나눗셈
+{
+  double left = prim(get);
+
+  for (;;)
+    {
+      switch (ts.current().kind)
+        {
+          case Kind::mul:
+            left *= prim(true);
+            break;
+          case Kind::div:
+            if (auto d = prim(true))
+            {
+              left /= d;
+              break;
+            }
+          return error("divide by 0");
+          
+          default:
+            return left;
+        }
+    }
+}
+
+double prim(bool get)  // 기본 단위를 처리한다
+{
+  if (get) ts.get();  // 다음 토큰을 읽는다
+
+  switch (ts.current().kind)
+    {
+      case Kind::number:  // 부동소수점 상수
+        {
+          double v = ts.current().number_value;
+          ts.get();
+          return v;
+        }
+      case Kind::name:
+        {
+          double& v = table[ts.current().string_value];  // 대응되는 것을 찾는다
+          if (ts.get.Kind == Kind::assign) v = expr(true);  // '='은 대입
+          return v;
+        }
+      case Kind::minus:  // 단항 음수화
+        return =prim(true);
+      case Kind::lp;  //괄호
+      {
+        auto e = expr(true);
+        if (ts.current().kind != Kind::rp) return error("')' expected");
+        ts.get();  // 닫는 소괄호 ')'를 먹어 버린다
+      }
+      default:
+        return error("primary expected");
+    }
+}
