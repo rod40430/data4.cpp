761053cb625591081335607ed97689f1d9fd4b97
xogns <xogns40430@naver.com>
2024-02-21 22:39:10 +0900

    간단한 람다와 펑터

---
+++ 람다표현식.cpp
@@ -0,0 +1,21 @@
+// 람다표현식
+void print_modulo(const vector<int>& v, ostream& os, int m)
+// v[i] % m == 0이면 v[i]를 os로 출력한다
+{
+  for_each(begin(v), end(v),
+    [&os, m](int x) {if (x % m == 0) os << x << " ";});
+}
+
+// 이것을 펑터로 표현한다면
+class Modulo_print
+{
+private:
+  ostream& os;  // 캡처 리스트를 보관할 멤버
+  int m;
+public:
+  Modulo_print(ostream& s, int mm) : os(s), m(mm) {}  // 캡처
+  void operator()(int x) const
+  {
+    if (x % m == 0) os << x << '\n';
+  }
+};
